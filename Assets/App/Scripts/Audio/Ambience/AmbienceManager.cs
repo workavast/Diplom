201be2529ece381de9d05b12be008637c340fc34
Rodion <workavast@yandex.ru>
2025-04-02 14:51:45 +0300

    ambince transition bug fix

--- Assets/App/Scripts/Audio/Ambience/AmbienceManager.cs
+++ Assets/App/Scripts/Audio/Ambience/AmbienceManager.cs
@@ -1,67 +1,78 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace App.Audio.Ambience
 {
     public class AmbienceManager : MonoBehaviour
     {
-        private AmbienceSource _currentAmbience;
+        private readonly List<AmbienceSource> _activeAmbiences = new(2);
         private Coroutine _transitionCoroutine;
-    
-        private void Awake() 
+
+        private void Awake()
             => DontDestroyOnLoad(gameObject);
 
-        public void Activate(AmbienceSource newAmbiencePrefab, float transitionTime = 1f)
+        public void Activate(AmbienceSource ambiencePrefab, float transitionTime = 1f)
         {
-            if (newAmbiencePrefab == null)
+            if (ambiencePrefab == null)
             {
-                Debug.LogError("You try activate null ambience");
+                Debug.LogError("You tried to activate a null ambience");
                 return;
             }
 
-            if (_currentAmbience != null && _currentAmbience.name == newAmbiencePrefab.name)
+            if (_activeAmbiences.Any(a => a.name == ambiencePrefab.name))
                 return;
 
-            if (_transitionCoroutine != null) 
+            if (_transitionCoroutine != null)
                 StopCoroutine(_transitionCoroutine);
-            
-            var newAmbience = Instantiate(newAmbiencePrefab, transform);
-            newAmbience.name = newAmbiencePrefab.name;
+
+            var newAmbience = Instantiate(ambiencePrefab, transform);
+            newAmbience.name = ambiencePrefab.name;
+            _activeAmbiences.Add(newAmbience);
+
             _transitionCoroutine = StartCoroutine(TransitionAmbience(newAmbience, transitionTime));
         }
-    
+
         private IEnumerator TransitionAmbience(AmbienceSource newAmbience, float duration)
         {
             var time = 0f;
-        
+
             var newSource = newAmbience.AudioSource;
             newSource.volume = 0f;
             newSource.Play();
 
-            AudioSource prevSource = null;
-            if (_currentAmbience != null) 
-                prevSource = _currentAmbience.AudioSource;
-            
-            var prevSourceVolume = prevSource != null ? prevSource.volume : 0f;
             var newSourceDefaultVolume = newAmbience.DefaultVolume;
-        
+
+            var fadingSources = new List<AudioSource>(_activeAmbiences.Count > 1 ? _activeAmbiences.Count - 1 : 0);
+            for (var i = 0; i < fadingSources.Capacity; i++) 
+                fadingSources.Add(_activeAmbiences[i].AudioSource);
+
+            // var fadingSources = _activeAmbiences
+            //     .Where(a => a != newAmbience)
+            //     .Select(a => a.AudioSource)
+            //     .ToList();
+
+            var initialVolumes = fadingSources.ToDictionary(s => s, s => s.volume);
+
             while (time < duration)
             {
                 time += Time.deltaTime;
                 var t = time / duration;
-            
-                if (prevSource != null)
-                    prevSource.volume = Mathf.Lerp(prevSourceVolume, 0f, t);
-            
+
+                foreach (var source in fadingSources)
+                    source.volume = Mathf.Lerp(initialVolumes[source], 0f, t);
+
                 newSource.volume = Mathf.Lerp(0f, newSourceDefaultVolume, t);
-            
+
                 yield return null;
             }
-        
-            if (_currentAmbience != null) 
-                Destroy(_currentAmbience.gameObject);
 
-            _currentAmbience = newAmbience;
+            foreach (var ambience in _activeAmbiences.Where(a => a != newAmbience).ToList())
+            {
+                Destroy(ambience.gameObject);
+                _activeAmbiences.Remove(ambience);
+            }
         }
     }
 }
